7b7a095a1682e890bb2a58261b1cfc89ffd67e4f
OmarAbdelmonem <omar2018170260@cis.asu.edu.eg>
2021-06-03 01:16:23 +0200

    adding admin-student and admin-instructor forms

---
+++ CourseSystem/CourseSystem/UserControl1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseSystem
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        #region MyRegion
+        private string _title;
+        private int _cost;
+
+        [Category("Custom Props")]
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; label1.Text = Title; }
+        }
+
+
+        [Category("Custom Props")]
+        public int Cost
+        {
+            get { return _cost; }
+            set { _cost = value; label3.Text = Cost.ToString(); }
+        }
+        #endregion
+    }
+}
