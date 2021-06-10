8d60d3e5e0b8dd51b3171171ea9ec555e15c1765
omar hafez fox <omerhafez778@gmail.com>
2021-06-10 23:53:56 +0200

    adding student forms

---
+++ CourseSystem/CourseSystem/courses_not_enrollment_form.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CourseSystem
+{
+    partial class courses_not_enrollment_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 496);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(775, 490);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // courses_not_enrollment_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 500);
+            this.Controls.Add(this.panel1);
+            this.Name = "courses_not_enrollment_form";
+            this.Text = "courses_not_enrollment_form";
+            this.Load += new System.EventHandler(this.courses_not_enrollment_form_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ CourseSystem/CourseSystem/main_window.cs
@@ -0,0 +1,145 @@
+﻿using CourseSystem.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace CourseSystem
+{
+    public partial class main_form : Form
+    {
+        
+        ///  connection to database 
+         string ordb = "Data Source=ORCL;User Id=hr;Password=hr;";
+         OracleConnection con;
+         OracleDataAdapter adapter;
+         OracleDataAdapter adapter2;
+
+        // to do 
+        string stud_id = "3";
+
+        public main_form()
+        {
+            InitializeComponent();
+        }
+
+        private void populateitems()
+        {
+
+            //return from databse all information that will display in the first window which displayed after student'login (main_window) 
+            con = new OracleConnection(ordb);
+            con.Open();
+            OracleCommand cmd_s = new OracleCommand();
+            cmd_s.Connection = con;
+            cmd_s.CommandText = "select username,balance,email,mobile_number from student where student_id=:stud_id";
+            cmd_s.CommandType = CommandType.Text;
+            cmd_s.Parameters.Add("id", stud_id);
+            adapter2 = new OracleDataAdapter(cmd_s);
+            DataTable all_data = new DataTable();
+            adapter2.Fill(all_data);
+            this.UserName.Text= all_data.Rows[0][0].ToString();
+            this.balance.Text= all_data.Rows[0][1].ToString();
+            this.email.Text= all_data.Rows[0][2].ToString();
+            this.mobile.Text= all_data.Rows[0][3].ToString();
+
+            
+            /* using this procedure to return the instructor_id 
+               which used to find the name of instructor and return 
+               all coursesname  that the student enrolled it to display it
+            */
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "return_enrolled_courses";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("student_id", stud_id);
+            cmd.Parameters.Add("data", OracleDbType.RefCursor, ParameterDirection.Output);
+            adapter = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            int count = dt.Rows.Count;
+            UserControl1[] listItems = new UserControl1[count];
+            // loop in all courses that student enrolled it to display 
+            for (int i = 0; i < listItems.Length; i++)
+            {
+                
+                
+                /* this command text to return the name of instructor to display it in 
+                   enrolled courses
+                */
+                OracleCommand cmd2 = new OracleCommand();
+                cmd2.Connection = con;
+                cmd2.CommandText = "select firstname,lastname from instructor where instrcutor_id=:numb";
+                cmd2.Parameters.Add("id", dt.Rows[i][0].ToString());
+                adapter = new OracleDataAdapter(cmd2);
+                DataTable dt2 = new DataTable();
+                adapter.Fill(dt2);
+               
+                // make object from usercontrol to display shape of courses
+                listItems[i] = new UserControl1();
+                listItems[i].Width = flowLayoutPanel1.Width;
+                listItems[i].Icon = Resources.courses;
+                listItems[i].Iconbackground = Color.Silver;
+                listItems[i].Title = dt.Rows[i][1].ToString();
+                listItems[i].HideButton();
+                listItems[i].Message ="Dr:"+dt2.Rows[0][0].ToString()+'_'+ dt2.Rows[0][1].ToString();
+               
+                if (flowLayoutPanel1.Controls.Count < 0)
+                {
+                    flowLayoutPanel1.Controls.Clear();
+                }
+                else 
+                flowLayoutPanel1.Controls.Add(listItems[i]);
+            }
+        }
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Show courses_not_enrollment_form
+            courses_not_enrollment_form co = new courses_not_enrollment_form();
+            co.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
